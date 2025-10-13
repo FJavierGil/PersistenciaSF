33e3fd2938bf793094d245036750342234b3a3fb
Javier Gil <franciscojavier.gil@upm.es>
2025-10-13 11:05:18 +0200

    Inicio Proyecto

--- app/src/main/java/es/upm/miw/persistenciasf/MainActivity.java
+++ app/src/main/java/es/upm/miw/persistenciasf/MainActivity.java
@@ -1,6 +1,9 @@
 package es.upm.miw.persistenciasf;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.os.Bundle;
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EdgeToEdge.enable(this);
+        // Establece las inserciones de recortes de pantalla
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            androidx.core.graphics.Insets systemBars = insets.getInsets(
+                    WindowInsetsCompat.Type.systemBars()
+                            | WindowInsetsCompat.Type.displayCutout());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
